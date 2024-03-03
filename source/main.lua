007b7816abddd874b2152a21181d6ad9bc28cc38
Stevie Goodman <mail@steviegoodman.com>
2024-03-03 18:34:57 +0000

    Added percentage to display

--- source/main.lua
+++ source/main.lua
@@ -1,17 +1,30 @@
 import "CoreLibs/graphics"
 
+local APPLICATION_TIME <const> = playdate.epochFromTime({
+	minute = 00,
+	hour = 10,
+	day = 9,
+	month = 2,
+	year = 2024,
+})
+local REMOVAL_TIME <const> = playdate.epochFromTime({
+	minute = 20,
+	hour = 9,
+	day = 26,
+	month = 3,
+	year = 2024,
+})
+
 local gfx <const> = playdate.graphics
 
-function getTimeLeft()
+function getTimeBetween(timeA, timeB)
 	local currentTime = playdate.getSecondsSinceEpoch()
-	local removalTime = playdate.epochFromTime({
-		minute = 20,
-		hour = 9,
-		day = 26,
-		month = 3,
-		year = 2024,
-	})
-	local secondsLeft = removalTime - currentTime
+	local secondsLeft = 0
+	if timeA > timeB then
+	    secondsLeft = timeA - timeB
+	else
+		secondsLeft = timeB - timeA
+	end
 	local daysLeft = math.floor(secondsLeft / 60 / 60 / 24)
 	secondsLeft -= daysLeft * 60 * 60 * 24
 	local hoursLeft = math.floor(secondsLeft / 60 / 60)
@@ -26,13 +39,32 @@ function getTimeLeft()
 	}
 end
 
+function getTimeLeft()
+	return getTimeBetween(REMOVAL_TIME, playdate.getSecondsSinceEpoch())
+end
+
+function getSecondsSince()
+	return playdate.getSecondsSinceEpoch() - APPLICATION_TIME
+end
+
+function getDurationInSeconds()
+	return REMOVAL_TIME - APPLICATION_TIME
+end
+
 function display()
 	playdate.graphics.drawTextAligned("*CAST COUNTER*", 200, 16, kTextAlignment.center)
-	playdate.graphics.drawTextAligned("You have", 200, 48, kTextAlignment.center)
+	
+	-- Time left
 	local timeLeft = getTimeLeft()
-	local timeLeftString = "*".. timeLeft.days .. "d " .. timeLeft.hours .. "h " .. timeLeft.minutes .. "m " .. timeLeft.seconds .. "s*"
-	playdate.graphics.drawTextAligned(timeLeftString, 200, 80, kTextAlignment.center)
-	playdate.graphics.drawTextAligned("left until the cast is removed!", 200, 112, kTextAlignment.center)
+	local timeLeftString = "*"..timeLeft.days.."d "..timeLeft.hours.."h "..timeLeft.minutes.."m "..timeLeft.seconds.."s*"
+	playdate.graphics.drawTextAligned("You have\n"..timeLeftString.."\nleft until the cast is removed!", 200, 42, kTextAlignment.center)
+	
+	-- Percentage
+	local percentage = math.floor(
+		(getSecondsSince() / getDurationInSeconds()) * 100
+	)
+	local percentageString = "That's *"..percentage.."%* of the way there!"
+	playdate.graphics.drawTextAligned(percentageString, 200, 96+32, kTextAlignment.center)
 end 
 
 function playdate.update()
